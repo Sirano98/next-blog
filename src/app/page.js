c7204376716ee50ce9f20867b3965ef85ee3de7d
Dmytro Protsiuk <dmytro@Dmytros-MacBook-Pro.local>
2025-01-24 21:19:34 +0200

    add: homePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+import Image from "next/image";
+import styles from "./home.module.css"
+
 export default function Home() {
     return (
-        <div>
-            <main>
-                Hello world
-            </main>
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+                <p className={styles.desc}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                </p>
+                <div className={styles.buttons}>
+                    <button className={styles.button}>Learn More</button>
+                    <button className={styles.button}>Contanct</button>
+                </div>
+                <div className={styles.brands}>
+                    <Image src="/brands.png" alt="brands" fill className={styles.brandImg} />
+                </div>
+            </div>
+            <div className={styles.imageContainer}>
+                <Image src="/hero.gif" alt="brands" fill className={styles.heroImg} />
+            </div>
         </div>
     );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,13 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>Logo</div>
+            <div className={styles.text}>
+                Creative thoughts agency
+            </div>
+        </div>
     )
 }
 
