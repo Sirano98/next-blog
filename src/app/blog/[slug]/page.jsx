550e3cd54bbeeaf0f49342538e6a4f740bf261a7
Dmytro Protsiuk <dmytro@Dmytros-MacBook-Pro.local>
2025-02-04 14:49:49 +0200

    add: fetching post cards and single posts

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,34 +1,39 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
+import { getPost } from "../../../../lib/data";
+import { Suspense } from "react";
+import PostUser from "@/components/postUser/postUser";
 
 const SinglePostPage = async ({ params }) => {
+    const { slug } = await params;
+    const post = await getPost(slug);
 
     return (
-        // <div className={styles.container}>
-        //     {post.img && (
-        //         <div className={styles.imgContainer}>
-        //             <Image src={post.img} alt="" fill className={styles.img} />
-        //         </div>
-        //     )}
-        //     <div className={styles.textContainer}>
-        //         <h1 className={styles.title}>{post.title}</h1>
-        //         <div className={styles.detail}>
-        //             {post && (
-        //                 <Suspense fallback={<div>Loading...</div>}>
-        //                     <PostUser userId={post.userId} />
-        //                 </Suspense>
-        //             )}
-        //             <div className={styles.detailText}>
-        //                 <span className={styles.detailTitle}>Published</span>
-        //                 <span className={styles.detailValue}>
-        //                     {post.createdAt.toString().slice(4, 16)}
-        //                 </span>
-        //             </div>
-        //         </div>
-        //         <div className={styles.content}>{post.desc}</div>
-        //     </div>
-        // </div>
-        <div>Single Post</div>
+        <div className={styles.container}>
+            {post.img && (
+                <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" fill className={styles.img} />
+                </div>
+            )}
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>{post.title}</h1>
+                <div className={styles.detail}>
+                    {post && (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <PostUser userId={post.userId} />
+                        </Suspense>
+                    )}
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Published</span>
+                        <span className={styles.detailValue}>
+                            {/* {post.createdAt.toString().slice(4, 16)} */}
+                            {12345}
+                        </span>
+                    </div>
+                </div>
+                <div className={styles.content}>{post.desc}</div>
+            </div>
+        </div>
     );
 };
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,16 +1,19 @@
+import PostCard from "@/components/postCard/postCard";
+import { getPosts } from "../../../lib/data";
 import styles from "./blog.module.css";
 
 const BlogPage = async () => {
 
+    const posts = await getPosts()
+
     return (
-        // <div className={styles.container}>
-        //   {posts.map((post) => (
-        //     <div className={styles.post} key={post.id}>
-        //       <PostCard post={post} />
-        //     </div>
-        //   ))}
-        // </div>
-        <div>Blog</div>
+        <div className={styles.container}>
+            {posts.map((post) => (
+                <div className={styles.post} key={post.id}>
+                    <PostCard post={post} />
+                </div>
+            ))}
+        </div>
     );
 };
 
